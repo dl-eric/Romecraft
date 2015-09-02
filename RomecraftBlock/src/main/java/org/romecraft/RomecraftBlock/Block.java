b77017ddcfe51afb67f93582a3a18ad45d111532
dl-eric <orangepamelo@gmail.com>
2015-09-02 14:23:33 -0400

    Add

---
+++ RomecraftBlock/src/main/java/org/romecraft/RomecraftBlock/Block.java
@@ -0,0 +1,34 @@
+package org.romecraft.RomecraftBlock;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Block extends JavaPlugin implements Listener
+{
+	@Override
+	public void onEnable()
+	{
+
+	}
+	@Override
+	public void onDisable()
+	{
+
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onDamage(EntityDamageByEntityEvent e)
+	{
+		Player p = (Player) e.getEntity();
+
+		if(e.getCause() == DamageCause.ENTITY_ATTACK && p.isBlocking())
+		{
+			e.setCancelled(true);
+		}
+	}
+}
