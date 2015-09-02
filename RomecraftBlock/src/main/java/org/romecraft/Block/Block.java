f442409e726d8cdf163945aa13f0172e55b3d56b
dl-eric <orangepamelo@gmail.com>
2015-09-02 17:55:13 -0400

    Update

--- RomecraftBlock/src/main/java/org/romecraft/Block/Block.java
+++ RomecraftBlock/src/main/java/org/romecraft/Block/Block.java
@@ -1,8 +1,8 @@
 package org.romecraft.Block;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
@@ -10,10 +10,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Block extends JavaPlugin implements Listener
 {
+	
 	@Override
 	public void onEnable()
 	{
-		
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
 	@Override
 	public void onDisable()
@@ -21,11 +22,11 @@ public class Block extends JavaPlugin implements Listener
 
 	}
 
-	@EventHandler(priority = EventPriority.HIGH)
+	@EventHandler(ignoreCancelled = true)
 	public void onDamage(EntityDamageByEntityEvent e)
 	{
 		Player p = (Player) e.getEntity();
-
+		
 		if(e.getCause() == DamageCause.ENTITY_ATTACK && p.isBlocking())
 		{
 			e.setCancelled(true);
