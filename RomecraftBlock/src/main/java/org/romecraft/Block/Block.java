f09f5ad5e9e7939990147fb279b9aa8295d50f7e
dl-eric <orangepamelo@gmail.com>
2015-09-03 17:02:49 -0400

    Add Timer

--- RomecraftBlock/src/main/java/org/romecraft/Block/Block.java
+++ RomecraftBlock/src/main/java/org/romecraft/Block/Block.java
@@ -4,8 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Block extends JavaPlugin implements Listener
@@ -21,15 +23,27 @@ public class Block extends JavaPlugin implements Listener
 	{
 
 	}
-
+	
+	@EventHandler
+	public void onClick(final PlayerInteractEvent e)
+	{
+		if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+		{
+			
+		}
+	}
+	
 	@EventHandler(ignoreCancelled = true)
 	public void onDamage(EntityDamageByEntityEvent e)
 	{
 		Player p = (Player) e.getEntity();
-		
+
 		if(e.getCause() == DamageCause.ENTITY_ATTACK && p.isBlocking())
 		{
-			e.setCancelled(true);
+			if (p.getLocation().getDirection().dot(e.getDamager().getLocation().getDirection()) < 0)
+			{
+				e.setCancelled(true);
+			}
 		}
 	}
 }
