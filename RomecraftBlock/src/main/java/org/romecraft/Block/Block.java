f9e116383885a77bed400f360cef3037d09ba43e
dl-eric <orangepamelo@gmail.com>
2015-09-04 01:05:40 -0400

    Finalize

--- RomecraftBlock/src/main/java/org/romecraft/Block/Block.java
+++ RomecraftBlock/src/main/java/org/romecraft/Block/Block.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-//import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerInteractEvent;
